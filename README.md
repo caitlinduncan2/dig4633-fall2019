806203e951dbcecda60c7d0ca2534c66d01e4787
Caitlin Duncan <duncan.caitlin17@knights.ucf.edu>
2019-09-25 15:37:41 -0400

    Lab 1
    Completed Lab 1

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ Template for course repository
 
 As you complete each lab, you should provide a description of your features and changes here.
 
-## VR Lab: TITLE HERE
+## VR Lab: Lab 1: VR
+
+For my 2 interactibles, I plan on placing corn in the field for the player to pick and put into a basket. They will then bring the basket to the house. 
---
+++ in-class/wk1/Assets/_Scripts/ColliderScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ColliderScript : MonoBehaviour
+{
+    private Rigidbody rb;
+    public Text countText;
+
+    private int count;
+
+    public Text winText;
+
+    private void Start()
+    {
+        winText.text = "";
+        count = 0;
+        SetCountText();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+            if (other.gameObject.CompareTag("Basket"))
+            {
+                count = count + 1;
+                SetCountText();
+            }
+            if (count == 4)
+            {
+                winText.text = "Yummy! Good Job!";
+            }
+        
+    }
+
+    void SetCountText()
+    {
+    countText.text = "Corn: " + count.ToString();
+}
+
+
+
+}
